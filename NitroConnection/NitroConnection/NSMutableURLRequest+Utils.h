4f14381d17a1086cab5230bab3fe901cdaac3cc2
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-09-04 16:37:10 -0300

    Adding a category to generate a NSMutableURLRequest based on another NSURLRequest #3

---
+++ NitroConnection/NitroConnection/NSMutableURLRequest+Utils.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableURLRequest+Utils.h
+//  NitroConnection
+//
+//  Created by Daniel L. Alves on 4/9/14.
+//  Copyright (c) 2014 Daniel L. Alves. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableURLRequest( Utils_NitroConnection )
+
++( instancetype )requestWithRequest:( NSURLRequest * )request;
+
+@end
